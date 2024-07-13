d3547f288973bdb287f2e2cf1f2db40b98f3114e
lchenri <lucasbonicardoso@outlook.com>
2024-07-13 00:54:51 -0300

    serializer adicionado

---
+++ api/serializers.py
@@ -0,0 +1,8 @@
+from rest_framework import serializers
+from .models import Equipamento
+
+
+class EquipamentoSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Equipamento
+        fields = '__all__'
