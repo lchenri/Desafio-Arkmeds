7675e8a15a02b20aadd58eff8d4c90680d9b0ad0
lchenri <lucasbonicardoso@outlook.com>
2024-07-13 15:48:46 -0300

    views refatorada

--- api/views.py
+++ api/views.py
@@ -6,9 +6,6 @@ from api.models import Equipamento
 from api.serializers import EquipamentoSerializer
 
 
-# import serializer aqui
-
-
 @api_view(['GET', 'POST'])
 def manager_list_all_add_new(request):
     if request.method == 'GET':
@@ -18,27 +15,30 @@ def manager_list_all_add_new(request):
     elif request.method == 'POST':
         serializer_post = EquipamentoSerializer(data=request.data)
 
-        if serializer_post.is_valid():
-            serializer_post.save()
+        if not serializer_post.is_valid():
+            return Response("A request não está completa", status=status.HTTP_400_BAD_REQUEST)
 
+        serializer_post.save()
         return Response(serializer_post.data, status=status.HTTP_201_CREATED)
 
 
 @api_view(['GET', 'PUT', 'DELETE'])
 def manager_read_update_delete(request, pk):
-    if request.method == 'GET':
+    try:
         equipamento = Equipamento.objects.get(pk=pk)
+    except Equipamento.DoesNotExist:
+        return Response("Equipamento não encontrado no sistema", status=status.HTTP_404_NOT_FOUND)
+
+    if request.method == 'GET':
         serializer = EquipamentoSerializer(equipamento, many=False)
         return Response(serializer.data)
     elif request.method == 'PUT':
-        equipamento = Equipamento.objects.get(pk=pk)
         serializer = EquipamentoSerializer(instance=equipamento, data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        if not serializer.is_valid():
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        serializer.save()
+        return Response(serializer.data)
     elif request.method == 'DELETE':
-        equipamento = Equipamento.objects.get(pk=pk)
         equipamento.delete()
         return Response("Item deletado com sucesso!", status=status.HTTP_204_NO_CONTENT)
     return Response(status=status.HTTP_405_METHOD_NOT_ALLOWED)
