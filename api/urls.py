7876416a8292a4079da2dad688195d5c8d73d324
lchenri <lucasbonicardoso@outlook.com>
2024-07-16 15:25:56 -0300

    remoção do format_suffix_pttrns pq todas as requisições feitas para a API serao em JSON

--- api/urls.py
+++ api/urls.py
@@ -1,6 +1,5 @@
 from django.urls import path
 from . import views
-from rest_framework.urlpatterns import format_suffix_patterns
 
 
 urlpatterns = [
@@ -8,5 +7,3 @@ urlpatterns = [
     path('<int:pk>/', views.EquipamentoDetail.as_view(), name='read_update_delete'),
 ]
 
-
-urlpatterns = format_suffix_patterns(urlpatterns)
