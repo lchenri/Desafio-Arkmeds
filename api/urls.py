971fac50980e533aaa7682d3097073fa99bd9750
lchenri <lucasbonicardoso@outlook.com>
2024-07-16 15:20:19 -0300

    Views da api refatoradas para class based views

--- api/urls.py
+++ api/urls.py
@@ -1,7 +1,12 @@
 from django.urls import path
 from . import views
+from rest_framework.urlpatterns import format_suffix_patterns
+
 
 urlpatterns = [
-    path('', views.manager_list_all_add_new, name='read_create'),
-    path('<int:pk>/', views.manager_read_update_delete, name='read_update_delete'),
+    path('', views.EquipamentoList.as_view(), name='read_create'),
+    path('<int:pk>/', views.EquipamentoDetail.as_view(), name='read_update_delete'),
 ]
+
+
+urlpatterns = format_suffix_patterns(urlpatterns)
